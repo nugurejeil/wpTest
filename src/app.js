93c75424dad940945dbbe0799b8bf72d04796bef
nugurejeil <nugurejeil1@gmail.com>
2023-10-05 16:30:25 +0900

    20231005업로드

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import './style.css';
-import rabbit from './rabbit.png';
+import bg from './background.png';
+
+let env;
+
+if (process.env.NODE_ENV === 'development') {
+    env = dev;
+} else {
+    env = pro;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.innerHTML = `<img src="${rabbit}"/>`;
+    document.body.innerHTML = `<img src="${bg}"/>`;
 })
